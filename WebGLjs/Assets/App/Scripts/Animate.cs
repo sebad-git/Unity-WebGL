5c3c15f47c2681c03e3f0dba31390f0a87d2e853
Sebastian <69809923+sebad-git@users.noreply.github.com>
2020-09-24 15:44:59 -0300

    Initial Commit

---
+++ WebGLjs/Assets/App/Scripts/Animate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+
+
+    private Animator animator;
+    void Start() {
+        this.animator=GetComponent<Animator>();
+        this.animator.applyRootMotion=false;
+    }
+
+    public void Idle(){ this.animator.SetInteger("state",0); }
+    
+    public void Run(){ this.animator.SetInteger("state",1); }
+
+    public void Wave(){ this.animator.SetInteger("state",2); }
+    
+    public void Dance(){ this.animator.SetInteger("state",3); }
+    
+}
---
+++ WebGLjs/Assets/App/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public Animate character;
+
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKey(KeyCode.W)){ character.GetComponent<Animate>().Wave(); }
+        if(Input.GetKey(KeyCode.R)){ character.GetComponent<Animate>().Run(); }
+        if(Input.GetKey(KeyCode.D)){ character.GetComponent<Animate>().Dance(); }
+        if(Input.GetKey(KeyCode.I)){ character.GetComponent<Animate>().Idle(); }
+    
+    }
+}
